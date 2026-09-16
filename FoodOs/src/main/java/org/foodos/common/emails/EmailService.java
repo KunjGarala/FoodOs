5fc1df558de8932fd93ed94691f9aa8f32be0108
Kunj Garala <kunjgarala55@gmail.com>
2026-09-16 23:09:52 +0530

    Redy For Deployment

--- FoodOs/src/main/java/org/foodos/common/emails/EmailService.java
+++ FoodOs/src/main/java/org/foodos/common/emails/EmailService.java
@@ -1,15 +1,19 @@
+
 package org.foodos.common.emails;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class EmailService {
 
     @Autowired
@@ -18,17 +22,22 @@ public class EmailService {
     @Value("${spring.mail.properties.domain_name:foodos@example.com}")
     String from;
 
+    @Async
     public void sendEmail(String to, String subject, String body) {
-        // Implementation for sending email
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(to);
-        mailMessage.setSubject(subject);
-        mailMessage.setText(body);
-        mailMessage.setFrom(from);
-        javaMailSender.send(mailMessage);
-
+        try {
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setTo(to);
+            mailMessage.setSubject(subject);
+            mailMessage.setText(body);
+            mailMessage.setFrom(from);
+            javaMailSender.send(mailMessage);
+            log.info("Email sent successfully to {}", to);
+        } catch (Exception e) {
+            log.error("Failed to send email to {}: {}", to, e.getMessage(), e);
+        }
     }
 
+    @Async
     public void sendHtmlEmail(String to, String subject, String htmlBody) {
         try {
             MimeMessage message = javaMailSender.createMimeMessage();
@@ -41,9 +50,10 @@ public class EmailService {
             helper.setText(htmlBody, true);
 
             javaMailSender.send(message);
+            log.info("HTML email sent successfully to {}", to);
 
         } catch (MessagingException e) {
-            throw new RuntimeException("Failed to send email", e);
+            log.error("Failed to send HTML email to {}: {}", to, e.getMessage(), e);
         }
     }
 
